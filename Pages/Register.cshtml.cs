e62efd1c2d3930cbd3217120916ecd22581f9e7c
Sonata <sonata.sonaciukas@gmail.com>
2022-07-15 17:54:08 -0400

    Create register Model

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NoteBoard.Entities;
 
 namespace NoteBoard.Pages
 {
     public class RegisterModel : PageModel
     {
+        public Register Model { get; set; }
         public void OnGet()
         {
         }
