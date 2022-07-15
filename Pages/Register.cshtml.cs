63c472ef157e355d2574ec28deeca9e5aa2d3a39
Sonata <sonata.sonaciukas@gmail.com>
2022-07-15 17:09:46 -0400

    create Register

---
+++ Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoteBoard.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
