877b40d80be5cb21bf4c57e90984670ce02626de
Sonata <sonata.sonaciukas@gmail.com>
2022-07-16 11:47:27 -0400

    Created Register Db

---
+++ DbContexts/AuthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NoteBoard.Entities;
+
+namespace NoteBoard.DbContexts
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public DbSet<Register> Registers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=localhost;DataBase=Auth;Trusted_Connection=true;");
+        }
+    }
+}
--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -1,14 +1,55 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NoteBoard.Entities;
+using System.Threading.Tasks;
 
 namespace NoteBoard.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        
+
         public Register Model { get; set; }
+       
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> sigInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = sigInManager;
+         
+        }
+
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = Model.Email,
+                    Email = Model.Email
+
+                };
+                var result = await userManager.CreateAsync(user, Model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return Page();
         }
     }
 }
